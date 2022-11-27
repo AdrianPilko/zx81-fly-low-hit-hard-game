059cc1e10ca8a2181b967d007182a091f32de605
AdrianPilko <50658445+AdrianPilko@users.noreply.github.com>
2022-11-27 09:37:58 +0000

    further optimised and changed ship to two

--- fly.asm
+++ fly.asm
@@ -9,7 +9,8 @@
 ;;       include fire button command and make ship show fireing cannon when pressed
 ;;       add enemys, both ground luanched vertically and from right in air
 ;;       add collision detection and end of game
-
+;;
+;;      BUGS  : the "ship" character flickers but only when not on the top line, probably due to the "scrolling left the readd ship"
 
 #include "zx81defs.asm" ;; https://www.sinclairzxworld.com/viewtopic.php?t=2186&start=40
 ;EQUs for ROM routines
@@ -37,8 +38,11 @@
 ;D_FILE is location of screen memory (which moves depending on length of basic, but should be fixed after program is loaded
 ; probably should run some code to detect if this is 1K or 16K as well, or just have 2 verisons 1K and 16K
 #define D_FILE 16396
-;black block
-#define SHIP_CHARACTER_CODE 128  
+
+;ship is made up of two blocks
+#define SHIP_CHARACTER_CODE 129   ; left facing corner block
+#define SHIP_CHARACTER_CODE_1 130 ; right facing corner  
+
 ;black grey block
 #define GROUND_CHARACTER_CODE 8
 
@@ -135,80 +139,80 @@ to_print .equ to_print_mem ;use printByte16
 	
 ;set b to row, c to col	
 printByte 		;;http://swensont.epizy.com/ZX81Assembly.pdf?i=1
-	PUSH AF ;store the original value of A for later
-	
-	CALL PRINTAT ;
-	POP AF 
-	PUSH AF ;store the original value of A for later
-	AND $F0 ; isolate the first digit
-	RRA
-	RRA
-	RRA
-	RRA
-	ADD A,$1C ; add 28 to the character code
-	CALL PRINT
-	POP AF ; retrieve original value of A
-	AND $0F ; isolate the second digit
-	ADD A,$1C ; add 28 to the character code
-	CALL PRINT
-	RET
+    PUSH AF ;store the original value of A for later
+
+    CALL PRINTAT ;
+    POP AF 
+    PUSH AF ;store the original value of A for later
+    AND $F0 ; isolate the first digit
+    RRA
+    RRA
+    RRA
+    RRA
+    ADD A,$1C ; add 28 to the character code
+    CALL PRINT
+    POP AF ; retrieve original value of A
+    AND $0F ; isolate the second digit
+    ADD A,$1C ; add 28 to the character code
+    CALL PRINT
+    RET
 
 introWaitLoop
-	ld bc,$00ff ;max waiting time
+    ld bc,$00ff ;max waiting time
 introWaitLoop_1
-	dec bc
-	ld a,b
-	or c
-	jr nz, introWaitLoop_1
-	jp read_start_key
-	
+    dec bc
+    ld a,b
+    or c
+    jr nz, introWaitLoop_1
+    jp read_start_key
+
 	
 setHighScoreZero
-	xor a
-	ld (high_score_mem_tens), a
-	ld (high_score_mem_hund), a
-	ld (last_score_mem_tens), a
-	ld (last_score_mem_hund), a	
+    xor a
+    ld (high_score_mem_tens), a
+    ld (high_score_mem_hund), a
+    ld (last_score_mem_tens), a
+    ld (last_score_mem_hund), a	
 	
 	
 intro_title
-	call CLS	
-	ld bc,1
-	ld de,title_screen_edge
-	call printstring	
-	ld bc,34
-	ld de,title_screen_edge
-	call printstring		
-	ld bc,105
-	ld de,title_screen_txt
-	call printstring
-	ld bc,203
-	ld de,keys_screen_txt
-	call printstring	
-    
-    ld bc,235
-	ld de,keys_screen_txt_2
+    call CLS	
+    ld bc,1
+    ld de,title_screen_edge
+    call printstring	
+    ld bc,34
+    ld de,title_screen_edge
+    call printstring		
+    ld bc,105
+    ld de,title_screen_txt
     call printstring
-	ld bc,436
-	ld de,last_Score_txt
-	call printstring	
-	ld b, 14			; b is row to print in
-	ld c, 13			; c is column
+    ld bc,203
+    ld de,keys_screen_txt
+    call printstring	
+
+    ld bc,235
+    ld de,keys_screen_txt_2
+    call printstring
+    ld bc,436
+    ld de,last_Score_txt
+    call printstring	
+    ld b, 14			; b is row to print in
+    ld c, 13			; c is column
     ld a, (last_score_mem_hund) ; load hundreds
-	call printByte    
-	ld b, 14			; b is row to print in
-	ld c, 15			; c is column
-	ld a, (last_score_mem_tens) ; load tens		
-	call printByte	
-	ld bc,570	
-	ld de,credits_and_version_2
-	call printstring		
-	ld bc,727
-	ld de,title_screen_edge
-	call printstring		
-	ld bc,760
-	ld de,title_screen_edge
-	call printstring	
+    call printByte    
+    ld b, 14			; b is row to print in
+    ld c, 15			; c is column
+    ld a, (last_score_mem_tens) ; load tens		
+    call printByte	
+    ld bc,570	
+    ld de,credits_and_version_2
+    call printstring		
+    ld bc,727
+    ld de,title_screen_edge
+    call printstring		
+    ld bc,760
+    ld de,title_screen_edge
+    call printstring	
     ld c, $1f
     xor a
     out (c),a
@@ -219,69 +223,69 @@ read_start_key
     ; read fire button to start , works on real zx81 but not on EightyOne emulator
     ; comment out for version on github until work out a way of stopping EightyOne
     ; always returning bit set
-	ld a, KEYBOARD_READ_PORT_ENTER_TO_H	
-	in a, (KEYBOARD_READ_PORT)					; read from io port	
-	bit 3, a									; check  key pressed
+    ld a, KEYBOARD_READ_PORT_ENTER_TO_H	
+    in a, (KEYBOARD_READ_PORT)					; read from io port	
+    bit 3, a									; check  key pressed
     jp nz, carryOnCheckingStart
-    
+
     xor a
     ld (var_keys_or_joystick), a    ; zero the flag for using joystick, so use keys
     ld bc,298
-	ld de,using_joystick_string
+    ld de,using_joystick_string
     call printstring    
-    
+
     
 carryOnCheckingStart 
     ld a, (var_keys_or_joystick)    ; keys = 1
     and 1
     jp nz, dont_check_fire_button
-    
+
     ld b, 00010000b   ; 16 decimal, fire button
     in a,($1F)       ; a now has the input byte from the port 1f (which is the joystick port)
     and b 
     jp nz, main
     
 dont_check_fire_button    
-   
-	ld a, KEYBOARD_READ_PORT_A_TO_G	
-	in a, (KEYBOARD_READ_PORT)					; read from io port	
-	bit 1, a									; check S key pressed
+
+    ld a, KEYBOARD_READ_PORT_A_TO_G	
+    in a, (KEYBOARD_READ_PORT)					; read from io port	
+    bit 1, a									; check S key pressed
     jp nz, introWaitLoop
     ; this means that they pressed  s to start so only use keys
     ld a, 1
     ld (var_keys_or_joystick), a   ; keys = 1
 main
-	call CLS
+    call CLS
 
-	xor a 						; initialise score to zero, and 0 results in a equal to zero
-	ld (score_mem_tens),a	
-	ld (score_mem_hund),a
-	ld (score_mem_thou),a	    
-    
-	ld bc, $00ff					; set initial difficulty
-	ld (speedUpLevelCounter), bc
-	   
+    xor a 						; initialise score to zero, and 0 results in a equal to zero
+    ld (score_mem_tens),a	
+    ld (score_mem_hund),a
+    ld (score_mem_thou),a	    
+
+    ld bc, $00ff					; set initial difficulty
+    ld (speedUpLevelCounter), bc
+       
     ;ld b, COL_IN_SCREEN         ; initialise loop counter (used by djnz)       
     ld hl, 0
     ld (numberOfGroundBlocksMoved),hl
-   
+
     ld de, groundLevelMemory
     ld (groundLevelMemoryLocationNow), de
     ld b, 31       ; initialise loop counter (used by djnz)       
-    
-    
+
+
     
 initialiseGround        
-                ;; we use a pre initialise memory blcok to control the height of each 
-                ;; ground level. The far right column is drawn first then "scrolled" left 
-                ;; on each update of the game loop. The idea is to have multiple banks of
-                ;; landscape which can be used as different "game levels"        
-                
-                
-                ;; the aim of the next 8 lines is to get register "hl" to have the 
-                ;; thing that register "de" points to, but due to limits of z80, 
-                ;; have to use register indirect via two ld a, (de) !!!
-                ;; unless I'm missing something!! :-/
+    ;; we use a pre initialise memory blcok to control the height of each 
+    ;; ground level. The far right column is drawn first then "scrolled" left 
+    ;; on each update of the game loop. The idea is to have multiple banks of
+    ;; landscape which can be used as different "game levels"        
+
+
+    ;; the aim of the next 8 lines is to get register "hl" to have the 
+    ;; thing that register "de" points to, but due to limits of z80, 
+    ;; have to use register indirect via two ld a, (de) !!!
+    ;; unless I'm missing something!! :-/
     ld de, (groundLevelMemoryLocationNow)
     ld a,(de)      
     ld l, a        
@@ -290,49 +294,38 @@ initialiseGround
     ld h, a    
     inc de   ; increment again to get to the next location
     ld (groundLevelMemoryLocationNow), de
-    
+
     push hl         ;; you can't just do "ld hl, de", that instruction doesn't exist, 
-                    ;; would be too easy, so have to use stack "push and pop"
+    ;; would be too easy, so have to use stack "push and pop"
     pop de
 
     ld hl,(D_FILE) 
 
     add hl,de       ;; add the start of display memory to the offset 
-                    ;; (note the groundLevelMemory already has the plus 1 added to push one after D_FILE)
-  
+    ;; (note the groundLevelMemory already has the plus 1 added to push one after D_FILE)
+
     ld a, GROUND_CHARACTER_CODE
     ld (hl),a    
-    
-	djnz initialiseGround    ; djnz decrements b and jumps if not zero
 
-	;;;;;;;;;;;;;;;initialise ship
-	ld hl,(D_FILE) 
-	ld de, SHIP_SCREEN_MEM_START_OFFSET
-	add hl, de	
-	ld a,SHIP_CHARACTER_CODE 
-	ld (hl),a
-	ld (var_ship_pos),hl ;save ship posn
+    djnz initialiseGround    ; djnz decrements b and jumps if not zero
+
+    ;;;;;;;;;;;;;;;initialise ship
+    ld hl,(D_FILE) 
+    ld de, SHIP_SCREEN_MEM_START_OFFSET
+    add hl, de	
+    ld a,SHIP_CHARACTER_CODE 
+    ld (hl),a
+    ld (var_ship_pos),hl ;save ship posn
+    inc hl
+    ld a,SHIP_CHARACTER_CODE_1
+    ld (hl),a          
     
     ld a, 10 
     ld (vertPosition),a
-
-
-    ; test add character at bottom right (remove later)
-     
-    ;ld hl,(D_FILE) 
-	;ld de, 725
-	;add hl, de	
-	;ld a,128 
-	;ld (hl),a 
     
 mainGameLoop
 
-    ;; scroll ground left
-
-    ld hl,(var_ship_pos) ; var_ship_pos already has the D_FILE offset added    
-    inc hl
-	ld a,SHIP_CHARACTER_CODE 
-	ld (hl),a              
+    ;; scroll ground left       
        
     ld b, 22
     ld hl,(D_FILE)          
@@ -342,63 +335,60 @@ screen_scroll_left_row
     push bc    
     ld b, 31
     ld c, 0
-    
+
     ld a, 1                  ; a is set to zero (z80 fastest way to zero a number (4 clock cycles (T-states))http://z80-heaven.wikidot.com/instructions-set:xor#toc4
     ld (col_counter), a    ; store column count
+
+    ld hl,(currentRowOffset) ; get the value at row and column pos + 1, then move to current row and column
+    ld de, (col_counter)
+    add hl, de
     
 screen_scroll_left_col           
   
     ; the idea is to go from left to right of screen moving each block at it's current location to the left by 1
-    ; do each column by row
-    ld hl,(currentRowOffset) ; get the value at row and column pos + 1, then move to current row and column
-    ld de, (col_counter)
-    add hl, de
-    ld a, (hl)
-    ld (screen_content_at_current), a    
-        
-    dec hl
+    ; do each column by row   
+    ld a, (hl)  
+    dec hl          ; move the index to screen memory left by one    
+    ld (hl), a  ; deposit the value found from column + 1 (stored in a)  in column pointed to by the decremented hl    
+    inc hl
+    inc hl
 
-    ld a, (screen_content_at_current)   
-    ld (hl), a
-
-    ld a, (col_counter)    ; load column count
-    inc a                  ; increment the column count
-    ld (col_counter), a    ; store column count
-    
     djnz screen_scroll_left_col    
 
-;endDEBUG
-;    jp endDEBUG
-   ld hl,(currentRowOffset)
-   ld de, 33
-   add hl, de
-   ld (currentRowOffset), hl
-   pop bc
-   djnz screen_scroll_left_row
+
+    ld hl,(currentRowOffset)
+    ld de, 33
+    add hl, de
+    ld (currentRowOffset), hl
+    pop bc
+    djnz screen_scroll_left_row
 
     ; as we scrolled whole screen left, the ship has to be moved right one column to maintain it's position
     ;; even without user input
     ld hl,(var_ship_pos) ; var_ship_pos already has the D_FILE offset added    
-	ld a,SHIP_CHARACTER_CODE 
-	ld (hl),a          
+    ld a,SHIP_CHARACTER_CODE 
+    ld (hl),a          
+    inc hl
+    ld a,SHIP_CHARACTER_CODE_1
+    ld (hl),a          
 
-
-;; before we add the next ground block at the far right we need to zero the column to preven block 
-;;drag over all the columns
+    ;; before we add the next ground block at the far right we need to zero the column to preven block 
+    ;;drag over all the columns
     ld b, 20     
     ld hl,(D_FILE)
     inc hl
     ld de, 31   
     add hl, de	
+    
 zeroLastColumnLoop    	
-	xor a    ; character zero blank space
-	ld (hl),a
+    xor a    ; character zero blank space
+    ld (hl),a
     ld de, 33
     add hl,de
     djnz zeroLastColumnLoop    
 
 
-;;; now reuse some of the code from initialising the gorund in a one shot to draw next ground
+    ;;; now reuse some of the code from initialising the gorund in a one shot to draw next ground
     ld de, (groundLevelMemoryLocationNow)
     ld a,(de)      
     ld l, a        
@@ -407,7 +397,7 @@ zeroLastColumnLoop
     ld h, a    
     inc de   ; increment again to get to the next location
     ld (groundLevelMemoryLocationNow), de
-           
+
     push hl         ;; you can't just do "ld hl, de", that instruction doesn't exist, 
                     ;; would be too easy, so have to use stack "push and pop"
     pop de
@@ -418,21 +408,21 @@ zeroLastColumnLoop
 
     ld a, GROUND_CHARACTER_CODE
     ld (hl),a    
-    
+
     ; force remove the bottom left character which is being shift (and is score shifted)
     ld hl,(D_FILE) 
-	ld de, 694
-	add hl, de	
-	xor 0 
-	ld (hl),a 
+    ld de, 694
+    add hl, de	
+    xor 0 
+    ld (hl),a 
 
     ld hl,(numberOfGroundBlocksMoved)
     inc hl
     ld (numberOfGroundBlocksMoved), hl
     and a   
     or a          
-	ld de,-320
-	add hl,de    
+    ld de,-320
+    add hl,de    
     jr nc,afterCheckingGroundIndex
 
     jr resetGound   ; Yes I know it's the next line but  
@@ -440,23 +430,20 @@ zeroLastColumnLoop
 resetGound
     ld hl, 0 
     ld (numberOfGroundBlocksMoved),hl  
-    
+
     ld de, startOfNormalGround    
     ld (groundLevelMemoryLocationNow), de
-    
+
 afterCheckingGroundIndex      
     
-;;;;;;;;;;;;;;;    
-        
-
     ;; keyboard OR joystick input
-    
+
     ;; move ship up / down
     ld a, KEYBOARD_READ_PORT_SPACE_TO_B			
     in a, (KEYBOARD_READ_PORT)					; read from io port		
     bit 3, a					        ; N
     jp z, drawDown
-    
+
     ld a, KEYBOARD_READ_PORT_SHIFT_TO_V			
     in a, (KEYBOARD_READ_PORT)					; read from io port		
     bit 2, a					        ; X
@@ -470,22 +457,25 @@ drawDown
     inc a
     cp 22
     jp z, skipMove
-        
+
     ld a,(vertPosition)     ; the cp 1 mangles a so reload
     inc a
     ld (vertPosition),a
-  
+
     ;overrite existing ship pos with blank
     ld hl,(var_ship_pos)    
-	ld a, 0
-	ld (hl),a
-        
+    ld a, 0
+    ld (hl),a
+
     ld de,33
     add hl, de    
     ld (var_ship_pos), hl        
-	ld a,SHIP_CHARACTER_CODE     
-	ld (hl),a
-    
+    ld a,SHIP_CHARACTER_CODE     
+    ld (hl),a
+    inc hl
+    ld a,SHIP_CHARACTER_CODE_1
+    ld (hl),a          
+
     jp afterDrawUpDown
     
 drawUp
@@ -493,7 +483,7 @@ drawUp
     dec a
     cp 1
     jp z, skipMove
-    
+
     ld a,(vertPosition)     ; the cp 1 mangles a so reload
     dec a
     ld (vertPosition),a
@@ -501,59 +491,49 @@ drawUp
 
     ;overrite existing ship pos with blank
     ld hl, (var_ship_pos)
-	ld a, 0
-	ld (hl),a
-     
+    ld a, 0
+    ld (hl),a
+
     and a
     or a
     ld de,33    
     sbc hl, de    
 
     ld (var_ship_pos), hl        
-	ld a,SHIP_CHARACTER_CODE 
-	ld (hl),a
-
+    ld a,SHIP_CHARACTER_CODE 
+    ld (hl),a
+    inc hl
+    ld a,SHIP_CHARACTER_CODE_1
+    ld (hl),a          
 skipMove    
 afterDrawUpDown
-    
-
-    ;ld a, h
-    ;call hprint    
-    ;ld a, l
-    ;call hprint
-    ;LD A,_NL
-    ;CALL PRINT ;th _NL new line char
-
-
-
-
 
 preWaitloop
-	ld a,(score_mem_tens)				; add one to score, scoring is binary coded decimal (BCD)
-	add a,1	
-	daa									; z80 daa instruction realigns for BCD after add or subtract
-	ld (score_mem_tens),a	
-	cp 153
-	jr z, addOneToHund
-	jr skipAddHund
+    ld a,(score_mem_tens)				; add one to score, scoring is binary coded decimal (BCD)
+    add a,1	
+    daa									; z80 daa instruction realigns for BCD after add or subtract
+    ld (score_mem_tens),a	
+    cp 153
+    jr z, addOneToHund
+    jr skipAddHund
 addOneToHund
-	ld a, 0
-	ld (score_mem_tens), a
+    ld a, 0
+    ld (score_mem_tens), a
     ld a, (score_mem_hund)
-	add a, 1
-	daa
-	ld (score_mem_hund), a
+    add a, 1
+    daa
+    ld (score_mem_hund), a
 skipAddHund	
 
 printScoreInGame
-	ld b, 21		; b is row to print in
-	ld c, 1			; c is column
+    ld b, 21		; b is row to print in
+    ld c, 1			; c is column
     ld a, (score_mem_hund) ; load hundreds
-	call printByte    
-	ld b, 21			; b is row to print in
-	ld c, 3			; c is column
-	ld a, (score_mem_tens) ; load tens		
-	call printByte
+    call printByte    
+    ld b, 21			; b is row to print in
+    ld c, 3			; c is column
+    ld a, (score_mem_tens) ; load tens		
+    call printByte
 
     ;ld bc, (speedUpLevelCounter)
 	;ld hl, (speedUpLevelCounter)   ; makes it more difficult as you progress
